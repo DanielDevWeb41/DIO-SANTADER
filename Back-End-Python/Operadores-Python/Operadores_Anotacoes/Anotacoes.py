ab67e66a7b20b87cdbda4d4d55604b5c5c56593d
daniel <danielk15656@gmail.com>
2025-06-13 20:42:39 -0300

    primeiras aulas de operadores em python

---
+++ Back-End-Python/Operadores-Python/Operadores_Anotacoes/Anotacoes.py
@@ -0,0 +1,118 @@
+
+# OPERADORES ARITMETICOS
+
+# Adicao
+print(1 + 1)
+# >>> 2 
+
+# Subtracao
+print(10 - 2)
+# >>>> 8
+
+# Multiplicacao
+print(4 * 3 )
+# >>> 12
+
+# Divisao
+print(15 / 3)
+# >>> 5.0
+
+# Divisao Inteira
+print(12 // 2)
+# >>> 6
+
+# Modulo > RESTO DA DIVISAO
+print(10 % 3)
+# >>> 1
+
+# Exponenciacao
+print( 2 ** 3)
+# >>> 8
+
+
+
+# EXEMPLOS DE EXPRESSOES
+
+print (10 - 5 * 2)
+# >>>>> 0
+
+print((10 - 5) * 2)
+# >>>>> 10
+
+print(10 ** 2 * 2)
+# >>>> 200
+
+print(10 ** (2 *2))
+# >>>>>  10000
+
+print(10 / 2 * 4)
+# >>>>> 20.0
+
+
+
+
+# OPERADORES DE COMPARACAO
+
+#Igualdade 
+saldo = 450
+saque = 200
+print(saldo == saque)
+# >>>>> false
+
+
+#Diferenca
+print(saldo != saque)
+# >>>>> True
+
+#Maior que / maior ou igual
+print(saldo > saque)
+# >>>> True
+print(saldo >= saque)
+# >>>> True
+
+#Menor que / Menor ou igual
+print(saldo < saque)
+# >>>>>> False
+print( saldo <= saque)
+# >>>>> False
+
+
+
+
+
+
+# OPERADORES DE ATRIBUICAO
+
+#ATRIBUICAO SIMPLES
+saldo1 = 500
+print(saldo1)
+# >>>> 500
+
+#ATRIBUICAO COM ADICAO
+saldo1 += 200
+print(saldo1)
+# >>>>> 700
+
+#ATRIBUICAO COM SUBTRACAO
+saldo1 -= 100
+print(saldo1)
+# >>>>>> 600
+
+#ATRIBUICAO COM MULTIPLICACAO
+saldo1 *= 2 
+print(saldo1)
+# >>>>> 1200
+
+#ATRIBUICAO COM DIVISAO
+saldo1 //= 2
+print(saldo1)
+
+#ATRIBUICAO COM MODULO
+saldo1 %= 500
+print(saldo1)
+
+#ATRIBUICAO OM EXPONENCIACAO
+saldo1 **= 2
+print(saldo1)
+
+
